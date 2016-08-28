c870135e2c5fb3bc9bdc2f40ac8daa69a9b8cb91
tridon777 <tridon777@gmail.com>
2016-08-28 17:38:09 -0500

    bunch of misc

--- components/LeftPanel.tsx
+++ components/LeftPanel.tsx
@@ -10,13 +10,13 @@ export class LeftPanel extends React.Component<any, any> {
        return <Col xs={3} sm={3} md={3} lg={3} className="floatReset" >
                 <ListGroup className="subListGroup">
                     <LinkContainer to="/new">
-                        <ListGroupItem  header="Create a New Form"   ><Image src="/components/images/rightSize.png" responsive /></ListGroupItem>
+                        <ListGroupItem  header="Create a New Form"   ><a><Image src="/components/images/rightSize.png" responsive /></a></ListGroupItem>
                     </LinkContainer>
                     <LinkContainer to="/existing">
-                        <ListGroupItem id="existingForm"  header="View Existing Forms" ><Image src="/components/images/folderSize.png" responsive /></ListGroupItem>
+                        <ListGroupItem id="existingForm"  header="View Existing Forms" ><a><Image src="/components/images/folderSize.png" responsive /></a></ListGroupItem>
                      </LinkContainer>
                      <LinkContainer to="/view">
-                        <ListGroupItem id="viewDatabase"  header="View Database" ><Image src="/components/images/databaseSize.png" responsive /></ListGroupItem>
+                        <ListGroupItem id="viewDatabase"  header="View Database" ><a><Image src="/components/images/databaseSize.png" responsive /></a></ListGroupItem>
                     </LinkContainer>
                 </ListGroup>
             </Col>
--- components/MiddlePanel.tsx
+++ components/MiddlePanel.tsx
@@ -1,15 +1,45 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as $ from 'jquery';
 import { Row, Col, ListGroup, ListGroupItem, Image, Nav, NavItem } from "react-bootstrap";
 
 
 
+interface newer{
+    result: any;
+}
+
+export class NewForm extends React.Component<newer,any>{
+    public successCall(data: any){
+        console.log(data);
+    }
+
+    public AjaxCall(url: string, data: any,errorMessage: String, successCall: (data: any) => void){
+        let request = $.ajax({
+            type: "GET",
+            url: url,
+            data: data,
+            dataType: "json",
+            xhrFields: {withCredentials: false}
+
+        });
+
+        request.done(successCall);
+
+        request.fail(function (jqXHR:any, textStatus:any, errorThrown: SyntaxError)
+        {
+            console.log(textStatus);
+        });
+    }
 
 
-export class NewForm extends React.Component<any,any>{
+    componentDidMount(){
+        this.AjaxCall("http://localhost:3000/api/employee",{},"Something went wrong", this.successCall)
+    }
     render () {
+
        return <Col xs={3} sm={3} md={3} lg={3}  >
-                <div> new form </div>
+                <div> {this.props.result} </div>
               </Col>
     }
 }
